b772c44432e100efe7ad678fd51f63be66498530
JhonAlexanderRoperoMojica-ADSI <jropero111@gmail.com>
2022-09-29 11:13:18 -0500

    punto-5

--- main.js
+++ main.js
@@ -1,21 +1,26 @@
 addEventListener("DOMContentLoaded", (e) => {
     let formulario = document.getElementById('formulario');
     formulario.addEventListener("submit", (e) => {
-        e.preventDefault();
-        let perosnas = 40;
-        let mujeres = 1;
-        let hombres = 7;
-        let suma = 0;
+        let estudiantes = 2;
+        let contador = 1;
 
-        while (suma <= perosnas){
-            suma = mujeres + hombres;
-            mujeres+= 1;
-            hombres+= 1;
+        while (contador <= estudiantes){
+            let nota1 = parseFloat(prompt(`Inresa la primera nota del estudiante ${contador}`));
+            let nota2 = parseFloat(prompt(`Inresa la segunda nota del estudiante ${contador}`));
+            let nota3 = parseFloat(prompt(`Inresa la tercera nota del estudiante ${contador}`));
+            let nota4 = parseFloat(prompt(`Inresa la cuarta nota del estudiante ${contador}`));
+            let nota5 = parseFloat(prompt(`Inresa la quinta nota del estudiante ${contador}`));
 
-            alert(`La cantidad de mujeres es: ${mujeres}`);
-            alert(`La cantidad de hombres es: ${hombres}`);
-            alert(`La cantidad total de mujeres es: ${mujeres}`);
-            alert(`Cantidad de personas totales es: ${suma}`);
-        }
+            let nota = nota1 + nota2 + nota3 + nota4 + nota5;
+            let promedio = nota / 5;
+
+            if (promedio < 3){
+                alert(`El estudiante no puede presentar el examen su promedio fue: ${promedio}`);
+            }
+            else{
+                alert(`El estudiante puede presentar el examen su promedio fue: ${promedio}`);
+            }
+            contador ++;
+        } 
     })
 })
