7c9a5ba71a4f894f8144e12181809e5725c98ee2
JhonAlexanderRoperoMojica-ADSI <jropero111@gmail.com>
2022-09-29 10:31:47 -0500

    punto-3

--- main.js
+++ main.js
@@ -2,25 +2,18 @@ addEventListener("DOMContentLoaded", (e) => {
     let formulario = document.getElementById('formulario');
     formulario.addEventListener("submit", (e) => {
         e.preventDefault();
-        let procesos = 10;
+        let estudiantes = 5;
         let contador = 1;
-        let suma, resta, divicion, multiplicacion;
 
-        while (contador <= procesos){
-            let numero1 = parseInt(prompt(`Ingrese el primer Numero: `));
-            let numero2 = parseInt(prompt("Ingrese el segundo Numero: "));
-
-            suma = numero1 + numero2;
-            resta = numero1 - numero2;
-            divicion = numero1 / numero2;
-            multiplicacion = numero1 * numero2;
+        while (contador <= estudiantes){
+            let nombre = prompt(`Ingrese el nombre del estudiante ${contador}`);
+            let apellido = prompt(`Ingrese el apellido del estudiante ${contador}`);
+            let edad = parseInt(prompt(`Ingrese la edad del estudiante ${contador}`));
+            let promedio = parseFloat(prompt(`Ingrese el promedio del estudiante ${contador}`));
 
             contador+= 1;
 
-            alert(suma);
-            alert(resta);
-            alert(divicion);
-            alert(multiplicacion);
+            alert(`el promedio del estudiante llamado: ${nombre} es: ${promedio}`);
         }
     })
 })
