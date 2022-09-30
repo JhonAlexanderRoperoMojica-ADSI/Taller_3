04ba2564e01adcac014ed9f3059a9f20ec820dd0
Anju0307 <jropero111@gmail.com>
2022-09-29 19:42:52 -0500

    punto-7

--- main.js
+++ main.js
@@ -1,17 +1,28 @@
 addEventListener("DOMContentLoaded", (e) => {
     let formulario = document.getElementById('formulario');
     formulario.addEventListener("submit", (e) => {
-        let avion = 20;
-        let proyectil = 10;
+        let vendedor = parseInt(prompt("ingrese la cantidad de vendedores"));
         let contador = 1;
-        
-        while ((avion - proyectil) <= 10000){
-            avion = avion + 20;
-            proyectil = proyectil + 10;
-            contador+= 1;
 
-            alert(`La distancia del avion ${contador} es: ${avion}`);
-            alert(`La distancia del proyectil ${contador} es: ${proyectil}`);
+        while (contador <= vendedor) {
+            let sueldo = parseFloat(prompt("digite el sueldo"));
+            let Venta1 = parseFloat(prompt("ingrese la venta 1"));
+            let comision1 = Venta1 * 0.1;
+            let suma1 = Venta1 + comision1;
+            let Venta2 = parseFloat(prompt("ingrese la venta 2"));
+            let comision2 = Venta2 * 0.1;
+            let suma2 = Venta2 + comision2;
+            let Venta3 = parseFloat(prompt("ingrese la venta 3"));
+            let comision3 = Venta3 * 0.1;
+            let suma3 = Venta3 + comision3;
+            let total = suma1 + suma2 + suma3;
+            let Total_sueldo = sueldo + total
+
+            alert(`El sueldo del empleado es: ${sueldo}`);
+            alert(`Vendio un total de comiciones de: ${total}`);
+            alert(`El sueldo del empleado total es: ${Total_sueldo}`);
+            
+            contador ++;
         }
     })
 })
